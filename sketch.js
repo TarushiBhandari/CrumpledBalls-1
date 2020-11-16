c3e6da4ff813383dd3152f8da411581084c00d10
TarushiBhandari <72614463+TarushiBhandari@users.noreply.github.com>
2020-11-16 13:34:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var trash;
+var ground;
+var side1,side2,side3;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(600,680,1200,10);
+	trash= new Paper(80,200,20);
+    side1= new Dustbin(780,625,15,100);
+	side2= new Dustbin(697,667,150,15);
+	side3= new Dustbin(620,625,15,100);
+
+	Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  trash.display();
+  ground.display();
+  side1.display();
+  side2.display();
+  side3.display();
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+
+	if(keyCode===UP_ARROW){
+
+		Paper.position= trash.position;
+		Matter.Body.applyForce(trash.body,trash.body.position,{x:0.5,y:-1});
+		trash.isStatic= true;
+	}
+}
